e7b6556fcf5ab3df246825e0fd76d0ab958a19a2
marcial08 <aruquipamarcial08@gmail.com>
2023-01-06 18:10:22 -0400

    ADD-REFRESH

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ types.setTypeParser(1114, function(stringValue) {
 });
 
 const databaseConfig = {
-    'host': '10.0.1.155',
+    'host': '10.0.1.81',
     'port': 5432,
     'database': 'db_movil',
     'user': 'postgres',
-    'password': 'Idepro.159'
+    'password': 'gestion159'
 };
 
 const db = pgp(databaseConfig);
