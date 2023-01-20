4d37dc679f3fecf1c6de81361f852efe25ce2427
marcial08 <aruquipamarcial08@gmail.com>
2023-01-20 15:41:23 -0400

    ADD LABORATORIO

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ types.setTypeParser(1114, function(stringValue) {
 });
 
 const databaseConfig = {
-    'host': '10.0.1.81',
+    'host': '10.0.1.155',
     'port': 5432,
     'database': 'db_movil',
     'user': 'postgres',
-    'password': 'gestion159'
+    'password': 'Idepro.159'
 };
 
 const db = pgp(databaseConfig);
--- controllers/negociosController.js
+++ controllers/negociosController.js
@@ -325,4 +325,3 @@ module.exports = {
         }
     },
 }
-
